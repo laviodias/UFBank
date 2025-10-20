41890092da3aaedc523f9c1591713565d77f8b85
Lávio Vale <laviovale@gmail.com>
2025-10-19 21:04:19 -0300

    feat: add página de cadastro

---
+++ ufbank/src/app/cadastro/page.tsx
@@ -0,0 +1,9 @@
+export default function CadastroPage() {
+  return (
+    <main className="flex items-center justify-center min-h-screen">
+      <h1 className="text-2xl">Cadastro</h1>
+    </main>
+  );
+}
+
+
